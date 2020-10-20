cc69e900b9ea0a994285f6b75ba620ea6fdd3559
Jonathan Hanson <jonathan@exabyte.io>
2020-10-20 15:12:38 -0700

    Use a real unit test value, now that we have it running

--- tests/unit/test_pbs_python.py
+++ tests/unit/test_pbs_python.py
@@ -4,4 +4,4 @@ import pbs
 
 class TestPbsPythonUnit(unittest.TestCase):
     def test_test(self):
-        self.assertEquals(pbs.ATTR_q, 'shrug')
+        self.assertEquals(pbs.ATTR_q, 'destination')
