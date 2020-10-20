06e3d53b09c7d1b87bb4378cc59be323fae3d2df
Jonathan Hanson <jonathan@exabyte.io>
2020-10-20 14:28:17 -0700

    Skeleton refactor to a standard python package structure

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 from distutils import sysconfig
+
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
 
 site_packages_path = sysconfig.get_python_lib(prefix='./')
 
 setup(
     name='pbs-python',
-    packages=['pbs'],
-    version='4.4.2.1',
+    version='2020.10.19',
+    description='Openpbs/Torque Python interface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/exabyte-io/pbs-python',
     author='Radik Fattakhov',
     author_email='radikft@gmail.com',
-    description='openpbs/torque python interface',
+    license='LGPLv3',
     keywords=['pbs'],
+    packages=find_packages(exclude=['tests*']),
     data_files=[
         (site_packages_path, ['pbs.pth']),
         (os.path.join(site_packages_path, 'pbs'), ['_pbs.so'])
     ],
-    url='http://github.com/radik/pbs-python',
-    license='LGPLv3',
+    python_requires='>=2.7, <3',
     classifiers=[
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Operating System :: POSIX :: Other',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
-    ],
-    long_description='''\
-This is pip-package of pbs_python 4.4.1 (http://oss.trac.surfsara.nl/pbs_python)
-by  Bas van der Vlies (bas.vandervlies@surfsara.nl).
-Native extensions from this package compiled on CentOS 7 with PBS Torque 5
-installed on it and probably not compatible with other versions.
-'''
+    ]
 )
---
+++ tests/unit/test_pbs_python.py
@@ -0,0 +1,7 @@
+import unittest
+
+import pbs
+
+class TestPbsPythonUnit(unittest.TestCase):
+    def test_test(self):
+        self.AssertTrue(True)
